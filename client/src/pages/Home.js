36d5da32c90b96ac2e461530dec1f508286fad8e
Jessica Anocibar <jessicayaws@yahoo.com>
2020-12-03 13:50:13 -0700

    pulling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,19 +7,38 @@ import Reviews from "../components/Reviews/Index"
 function Projects() {
     return (
   <>
-  <a href="container-fluid" className="">
+  <a href="/create" className="container-fluid">
    <div className="bg-3 text-center">
      <h3 className="margin"></h3><br />
          <div className="row">
-             <div className="col-sm-4 ">
-                <h1>Holiday</h1>
-              </div>
-              <div className="col-sm-4 ">
-                 <h1>Holiday</h1>
-               </div>
-                <div className="col-sm-4 ">
-                  <h1>Holiday</h1>
-                </div>
+
+    <div className="card col-sm-4">
+     <img className="card-img-top" alt="..." />
+       <div className="card-body">
+         <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+      </div>
+    </div>
+
+  <div className="card col-sm-4" style={'width: 18rem;'}>
+     <img className="card-img-top" alt="..." />
+       <div className="card-body">
+         <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+    </div>
+  </div>
+
+  <div className="card col-sm-4">
+     <img className="card-img-top" alt="..." />
+       <div className="card-body">
+         <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+    </div>
+  </div>
+        
               </div>
           </div>
         </a>
