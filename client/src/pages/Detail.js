72e8592e8988b4404ab233e49b9010a9c495d79e
Taylor <taylor.hackbart@du.edu>
2020-12-03 12:52:57 -0800

    detail page

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,44 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { List, ListItem } from "../components/List";
-import Gift from "../components/Gift";
+
 
 const Detail = () => {
-  const [users, setUsers] = useState({});
-  const [user, setUser] = useState ({
+  const [user, setUser] = useState({
     name: "",
-    giftList: []
-  })
+    giftList: [],
+  });
   const params = useParams();
 
   useEffect(() => {
-    API.getUser(params.id)
-      .then((res) => {
-        
-        setUser(res.data)
-      console.log(res.data)
-      }
-     
-      )
-      // .catch((err) => console.log(err));
-  }, [] );
-
+    API.getUser(params.id).then((res) => {
+      setUser(res.data);
+      console.log(res.data);
+    });
+    // .catch((err) => console.log(err));
+  }, []);
 
   return (
     <Container fluid>
       <Row>
         <Col size="md-12">
-          <ListItem>{users.name}</ListItem>
-          <Gift>
-            <>
-              <p key={users._id}>{users.title}</p>
-              <p>{users.image}</p>
-              <p>{users.link}</p>
-            </>
-          </Gift>
+          <p>{user.name}</p>
+        
+            {user.giftList.map((gift) => (
+              <List key={gift.title}>
+                <ListItem>{gift.title}</ListItem>
+                <ListItem > <img src ={gift.image} alt="gift" ></img></ListItem>
+                <ListItem > <a href={gift.link} > Click to purchase </a></ListItem>
+              </List>
+            ))}
+       
         </Col>
       </Row>
     </Container>
