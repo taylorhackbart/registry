5ce194d0dd38b6450beb6aaba5b72d43dabfef43
Taylor <taylor.hackbart@du.edu>
2020-12-03 09:56:19 -0800

    nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ const Nav = () => {
 return (
   <>
 <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand">Navbar</a>
+  <a class="navbar-brand">GIFTR</a>
   <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
