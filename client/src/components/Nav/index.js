ad2db0854543aa0e7154d824b94429fa2da66c37
Taylor <taylor.hackbart@du.edu>
2020-12-03 10:28:35 -0800

    create page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,16 @@
 import React from "react"
-
+import "./style.css"
 
 
 const Nav = () => {
 
 return (
   <>
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light">
   <a class="navbar-brand">GIFTR</a>
   <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
   </form>
 </nav>
 </>
---
+++ client/src/pages/Create.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Gift from "../components/Gift"
+
+const Create = () => {
+  return (
+    <>
+      <Gift />
+    </>
+  );
+};
+
+export default Create;
