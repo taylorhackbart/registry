d246bc2011d7fc655a9bb15a4f74465274ac5767
Jessica Anocibar <jessicayaws@yahoo.com>
2020-12-03 13:56:05 -0700

    pulling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,7 +21,7 @@ function Projects() {
       </div>
     </div>
 
-  <div className="card col-sm-4" style={'width: 18rem;'}>
+  <div className="card col-sm-4">
      <img className="card-img-top" alt="..." />
        <div className="card-body">
          <h5 className="card-title">Card title</h5>
