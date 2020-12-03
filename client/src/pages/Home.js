466a0d8a0fe92fc2c0c5afa32a5bd914a0b2779b
bishank <chandbishank@gmail.com>
2020-12-03 14:31:40 -0700

    testing the page renderation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,10 +4,12 @@ import Reviews from "../components/Reviews/Index"
 
 
 
-function Projects() {
+
+
+function Home() {
     return (
         <>
-        <a href="project"></a>
+            <a href="project"></a>
             <div className="container-fluid bg-3 text-center">
                 <h3 className="margin"></h3><br />
                 <div className="row">
@@ -15,10 +17,10 @@ function Projects() {
                         <h1>Holiday</h1>
                     </div>
                     <div className="col-sm-4 ">
-                    <h1>Holiday</h1>
+                        <h1>Holiday</h1>
                     </div>
                     <div className="col-sm-4 ">
-                    <h1>Holiday</h1>
+                        <h1>Holiday</h1>
                     </div>
                 </div>
             </div>
@@ -28,4 +30,4 @@ function Projects() {
     )
 };
 
-export default Projects;
+export default Home;
