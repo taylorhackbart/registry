06f8b5d4c14c8cb6cada935f756f8f94fb7d70ed
Jessica Anocibar <jessicayaws@yahoo.com>
2020-12-03 12:49:18 -0700

    pulling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,31 @@
 import React from 'react';
 import './home.css';
 import Reviews from "../components/Reviews/Index"
-import { Card, Button } from 'react-bootstrap';
-
 
 
 
 function Projects() {
     return (
-    <div className="Projects">
-      <header className="Project-header">
-        <Card className= "mb-3" style={{ color: "#000"}} >
-      <Card.Img src="https://picsum.photos/300/200" />
-      <Card.Body>
-        <Card.Title>
-          Card example
-        </Card.Title>
-        <Card.Text>
-          This is an example of react bootstrap cards
-        </Card.Text>
-        <Button variant="primary">Read more</Button>
-      </Card.Body>
-    </Card>
+  <>
+  <a href="container-fluid">
+   <div className="bg-3 text-center">
+     <h3 className="margin"></h3><br />
+         <div className="row">
+             <div className="col-sm-4 ">
+                <h1>Holiday</h1>
+              </div>
+              <div className="col-sm-4 ">
+                 <h1>Holiday</h1>
+               </div>
+                <div className="col-sm-4 ">
+                  <h1>Holiday</h1>
+                </div>
+              </div>
+          </div>
+        </a>
 
         <Reviews />
-    </header>
-</div>
-        /* <a href="project"></a>
-            <div className="container-fluid bg-3 text-center">
-                <h3 className="margin"></h3><br />
-                <div className="row">
-                    <div className="col-sm-4 ">
-                        <h1>Holiday</h1>
-                    </div>
-                    <div className="col-sm-4 ">
-                    <h1>Holiday</h1>
-                    </div>
-                    <div className="col-sm-4 ">
-                    <h1>Holiday</h1>
-                    </div>
-                </div>
-            </div> */
+   </>
         
     )
 };
