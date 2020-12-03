e788d92deb62f3e513cb0d1eb2968641eefb931c
Matt Boggs <matt.david.boggs@gmail.com>
2020-12-03 10:39:19 -0700

    tweaking routes to reflect user file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const usersRoutes = require("./users");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/users", usersRoutes);
 
 module.exports = router;
